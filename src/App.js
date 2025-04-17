32f29c606e497c4bce9b8690d0a8f99250f38fa7
fsangregorio <franciscosangregorio@hotmail.com>
2025-04-17 19:25:54 -0400

    all components created and synced

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// Import necessary modules from React library
 
+// Import components for routing from react-router-dom library
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Import custom Navbar component
+import Navbar from './Components/Navbar/Navbar';
+import LandingPage from './Components/Landing_Page/Landing_Page';
+import SignUp from './Components/Sign_Up/Sign_Up';
+import Login from './Components/Login/Login';
+
+
+// Function component for the main App
 function App() {
+
+  // Render the main App component
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* Set up BrowserRouter for routing */}
+        <BrowserRouter>
+          {/* Display the Navbar component */}
+          <Navbar/>
+
+          {/* Set up the Routes for different pages */}
+          <Routes>
+          <Route path="/" element={<LandingPage/>}/>
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/Login" element={<Login />} />            
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
 
+// Export the App component as the default export
 export default App;
