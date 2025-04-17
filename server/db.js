c485b165d4159c3b0b62732696132606b1b00ce5
fsangregorio <franciscosangregorio@hotmail.com>
2025-04-17 12:20:31 -0400

    third commit

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:<eRCY5CvPAPClpBwvKkl95Do6>@172.21.173.5:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
